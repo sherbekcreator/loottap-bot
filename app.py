2c81f3404540ee8fd55ff13a4530814ff635d6f2
sherbekcreator <sherbekshavkatov4939@gmail.com>
2026-06-08 23:55:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,2 @@
-from flask import Flask, send_from_directory
-import threading
-import bot  # Bu bot.py faylingni chaqiradi
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory('.', path)
-
-# Botni fon rejimida yurgizish
-if __name__ == "__main__":
-    threading.Thread(target=bot.bot.infinity_polling).start()
-    app.run(host="0.0.0.0", port=10000)
+pyTelegramBotAPI==4.15.4
+Flask
